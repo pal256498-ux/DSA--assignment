ddbb89d83209da7d25971af14baec8e54ec1b05a
pal256498-ux <pal256498@gmail.com>
2026-08-06 15:26:13 +0530

    Update Assignment05.c

---
+++ Assignment05.c
@@ -0,0 +1,71 @@
+ /*  date o6/o8/2026
+ WACP to take input in an array 10 unordered number and
+apply QuickSort to order the array in ascending order.
+input values:- [8,2,10,6,5,12,18,9,4,11]*/
+
+
+
+
+#include <stdio.h>
+
+void quickSort(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
+
+int main()
+{
+    int arr[] = {8, 2, 10, 6, 5, 12, 18, 9, 4, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original Array:\n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    printf("\n\nSorted Array:\n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if(low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for(int j = low; j < high; j++)
+    {
+        if(arr[j] <= pivot)
+        {
+            i++;
+
+            // Swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // Place pivot in its correct position
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
